ddf13c347e76cf00fe2d05b22ea496223b0e52b3
fbansept <bansept.franck@gmail.com>
2024-05-22 11:22:23 +0200

    fix : ajout verification droit

--- modifier-produit.php
+++ modifier-produit.php
@@ -2,6 +2,7 @@
 
 include 'header-init.php';
 include 'produit-helper.php';
+include 'jwt-helper.php';
 
 if (!isset($_GET['id'])) {
     echo '{"message" : "il n\'y a pas d\'identiant dans l\'URL"}';
@@ -18,6 +19,28 @@ $json = $_POST['produit'];
 
 // Le convertit en objet PHP
 $produit = json_decode($json);
+
+
+//On recupère le produit dans la bdd
+$requete = $connexion->prepare("SELECT * FROM produit WHERE id = ?");
+$requete->execute([$idProduit]);
+$produitBdd = $requete->fetch();
+
+//si il n'y a pas de produit on retourne une erreur 404
+if (!$produitBdd) {
+    http_response_code(404);
+    echo '{"message" : "Ce produit produit n\'existe pas"}';
+    exit();
+}
+
+$utilisateurConnecte = extractJwtBody();
+
+//si l'utilisateur n'est pas administrateur en plus de ne pas etre le créateur du produit 
+if (!$utilisateurConnecte->admin && $utilisateurConnecte->id != $produitBdd['id_utilisateur']) {
+    http_response_code(403);
+    echo '{"message" : "Vous n\'êtes ni administrateur, ni créateur du produit"}';
+    exit();
+}
 
 
 $requete = $connexion->prepare("UPDATE produit SET 
