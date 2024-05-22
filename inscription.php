c1ae7ea584e265562993415d6de550e84306a7d3
fbansept <bansept.franck@gmail.com>
2024-05-22 11:57:14 +0200

    ajout verification email existant

--- inscription.php
+++ inscription.php
@@ -8,15 +8,24 @@ $json = file_get_contents('php://input');
 // Le convertit en objet PHP
 $utilisateur = json_decode($json);
 
-//TODO : verifier que le mot de passe et l'email ont les formats attendus
+//verifier l'email de l'utilisateur est unique
+$requete = $connexion->prepare("SELECT * FROM utilisateur WHERE email = :email");
+$requete->execute(["email" => $utilisateur->email]);
+$utilisateurExistant = $requete->fetch();
+
+if ($utilisateurExistant) {
+    http_response_code(409); //note : 409 = CONFLICT
+    echo '{"message" : "Cet email est déjà utilisé"}';
+    exit();
+}
 
 //On ajoute l'utilisateur dans la base de données
 $requete = $connexion->prepare("INSERT INTO utilisateur(email,password,admin) 
                                 VALUES (:email, :password, 0)");
 
-$requete->bindValue("email", $utilisateur->email);
-$requete->bindValue("password", password_hash($utilisateur->password, PASSWORD_DEFAULT));
-
-$requete->execute();
+$requete->execute([
+    "email" => $utilisateur->email,
+    "password" => password_hash($utilisateur->password, PASSWORD_DEFAULT)
+]);
 
 echo '{"message" : "Vous êtes inscrit"}';
